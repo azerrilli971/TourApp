d040196b14fcb5e54c2d4a0a4c3c9522056572df
Silvia <silmanca1@gmail.com>
2019-07-13 18:37:28 +0200

    Activity Landscape e nuovi menu con CardView

--- app/src/main/java/uniba/di/sms/ibtourapp/tourapp/InterestActivity.java
+++ app/src/main/java/uniba/di/sms/ibtourapp/tourapp/InterestActivity.java
@@ -1,11 +1,14 @@
 package uniba.di.sms.ibtourapp.tourapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.MenuItem;
+import android.view.View;
 
-public class InterestActivity extends AppCompatActivity {
-
+public class InterestActivity extends AppCompatActivity implements View.OnClickListener {
+    private CardView interessiFamiglieCv, interessiGiovaniCV, interessiLidiCV, interessiMuseiCV, interessiMonumentiCV, interessiChieseCV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,6 +16,21 @@ public class InterestActivity extends AppCompatActivity {
         //aggiunta tasto per tornare indietro
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        interessiChieseCV = findViewById(R.id.interessiChiese);
+        interessiFamiglieCv = findViewById(R.id.interessiFamiglie);
+        interessiGiovaniCV = findViewById(R.id.interessiGiovani);
+        interessiLidiCV = findViewById(R.id.interessiLidi);
+        interessiMonumentiCV = findViewById(R.id.interessiMonumenti);
+        interessiMuseiCV = findViewById(R.id.interessiMusei);
+
+        interessiMonumentiCV.setOnClickListener(this);
+        interessiLidiCV.setOnClickListener(this);
+        interessiGiovaniCV.setOnClickListener(this);
+        interessiFamiglieCv.setOnClickListener(this);
+        interessiChieseCV.setOnClickListener(this);
+        interessiMuseiCV.setOnClickListener(this);
+
     }
     @Override
     public boolean onOptionsItemSelected( MenuItem item){
@@ -22,5 +40,49 @@ public class InterestActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v){
+
+        Intent intent;
+        switch(v.getId()){
+
+            case R.id.interessiChiese:{
+                intent = new Intent(this, ChurchActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+            case R.id.interessiFamiglie:{
+                intent = new Intent(this, FamilyActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+            case R.id.interessiGiovani:{
+                intent = new Intent(this, YoungActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.interessiLidi:{
+                intent = new Intent(this, BeachActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+            case R.id.interessiMonumenti:{
+                intent = new Intent(this, MonumentActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+            case R.id.interessiMusei:{
+                intent = new Intent(this, MuseumActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+        }
     }
 }
--- app/src/main/java/uniba/di/sms/ibtourapp/tourapp/RestaurantActivity.java
+++ app/src/main/java/uniba/di/sms/ibtourapp/tourapp/RestaurantActivity.java
@@ -1,11 +1,16 @@
 package uniba.di.sms.ibtourapp.tourapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.MenuItem;
+import android.view.View;
 
-public class RestaurantActivity extends AppCompatActivity {
+//modificare Intent per reindirizzare alle activity corrette
+public class RestaurantActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private CardView mangiareGelaterieCV, mangiarePizzerieCV, mangiareRistorantiCV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,6 +18,15 @@ public class RestaurantActivity extends AppCompatActivity {
         //aggiunta tasto per tornare indietro
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        mangiareGelaterieCV = findViewById(R.id.mangiareGelaterie);
+        mangiarePizzerieCV = findViewById(R.id.mangiarePizzerie);
+        mangiareRistorantiCV = findViewById(R.id.mangiareRistoranti);
+
+        mangiareGelaterieCV .setOnClickListener(this);
+        mangiarePizzerieCV.setOnClickListener(this);
+        mangiareRistorantiCV.setOnClickListener(this);
+
     }
     @Override
     public boolean onOptionsItemSelected( MenuItem item){
@@ -22,5 +36,32 @@ public class RestaurantActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v){
+
+        Intent intent;
+        switch(v.getId()){
+
+            case R.id.mangiareGelaterie:{
+                intent = new Intent(this, GelaterieActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+            case R.id.mangiarePizzerie:{
+                intent = new Intent(this, PizzerieActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+            case R.id.mangiareRistoranti:{
+                intent = new Intent(this, RestaurantsActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+        }
     }
 }
