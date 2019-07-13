01b0336793b7d41cc28d6bb546dafe0f411a5afa
Alessandro Zerrilli <alessandrozerrilli@gmail.com>
2019-07-13 18:38:23 +0200

    Updated Login and Signup activities

--- app/src/main/java/uniba/di/sms/ibtourapp/tourapp/LoginActivity.java
+++ app/src/main/java/uniba/di/sms/ibtourapp/tourapp/LoginActivity.java
@@ -1,15 +1,29 @@
 package uniba.di.sms.ibtourapp.tourapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.textViewSignup).setOnClickListener(this);
+
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.textViewSignup:
+
+                startActivity(new Intent(this, SignupActivity.class));
+                break;
+        }
+    }
 }
