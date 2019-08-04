0d64cdda7f3cc8e14f64d785c8a4c4b995e3ed3c
Silvia <silmanca1@gmail.com>
2019-08-04 16:23:41 +0200

    raw navigation drawer senza listener

--- app/src/main/java/uniba/di/sms/ibtourapp/tourapp/ExploreActivity.java
+++ app/src/main/java/uniba/di/sms/ibtourapp/tourapp/ExploreActivity.java
@@ -1,24 +1,42 @@
 package uniba.di.sms.ibtourapp.tourapp;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
 public class ExploreActivity extends AppCompatActivity implements View.OnClickListener {
 
     private CardView esploraInteressiCV, esploraDormireCV, esploraMangiareCV;
-
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore);
 
+        toolbar = findViewById(R.id.toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        //setting toolbar as an actionbar
+        toolbar.setTitle(R.string.homeEsplora);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu_hamburger);
+        toolbar.setTitle(R.string.homeEsplora);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_closed );
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
         //aggiunta tasto per tornare indietro
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        /*getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);*/
 
         esploraDormireCV = findViewById(R.id.esploraDormire);
         esploraInteressiCV = findViewById(R.id.esploraInteressi);
@@ -31,13 +49,12 @@ public class ExploreActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     @Override
-    public boolean onOptionsItemSelected( MenuItem item){
-
-        if(item.getItemId() == android.R.id.home){
-            finish();
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
