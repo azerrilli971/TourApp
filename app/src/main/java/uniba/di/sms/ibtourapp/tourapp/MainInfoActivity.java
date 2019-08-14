e73f5955e2295ee7fbcec78a22427e7d55f8afcb
Silvia <silmanca1@gmail.com>
2019-08-14 18:19:16 +0200

    info main

--- app/src/main/java/uniba/di/sms/ibtourapp/tourapp/MainInfoActivity.java
+++ app/src/main/java/uniba/di/sms/ibtourapp/tourapp/MainInfoActivity.java
@@ -2,8 +2,10 @@ package uniba.di.sms.ibtourapp.tourapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 
 public class MainInfoActivity extends AppCompatActivity {
+    private CardView menuEsploraCV, menuDiarioCV, menuCouponCV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
