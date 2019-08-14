e7be4b0788991b3bcd988617726efa6811aea635
Silvia <silmanca1@gmail.com>
2019-08-14 12:15:36 +0200

    Menu hamburger interessi Menu hamburger hotel (aggiungere menu a layout land) modifica nav menu

--- app/src/main/java/uniba/di/sms/ibtourapp/tourapp/HotelActivity.java
+++ app/src/main/java/uniba/di/sms/ibtourapp/tourapp/HotelActivity.java
@@ -1,25 +1,45 @@
 package uniba.di.sms.ibtourapp.tourapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
 //aggiustare activity dell'intent
-public class HotelActivity extends AppCompatActivity implements View.OnClickListener{
+public class HotelActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener{
     private CardView dormireBBCV, dormireHotelCV;
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel);
-        //aggiunta tasto per tornare indietro
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         dormireBBCV = findViewById(R.id.dormireBB);
         dormireHotelCV= findViewById(R.id.dormireHotel);
 
+        toolbar = findViewById(R.id.toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        //setting toolbar as an actionbar
+        toolbar.setTitle(R.string.esploraDormire);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu_hamburger);
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_closed );
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
         dormireBBCV.setOnClickListener(this);
         dormireHotelCV.setOnClickListener(this);
     }
@@ -50,5 +70,41 @@ public class HotelActivity extends AppCompatActivity implements View.OnClickList
                 break;
             }
         }
+    }
+    @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        switch (id){
+
+            case R.id.nav_explore:
+                Intent h= new Intent(this,ExploreActivity.class);
+                startActivity(h);
+                break;
+            case R.id.nav_diary:
+                Intent i= new Intent(this,JournalActivity.class);
+                startActivity(i);
+                break;
+            case R.id.nav_coupon:
+                Intent g= new Intent(this,CouponsActivity.class);
+                startActivity(g);
+                break;
+
+
+        }
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
